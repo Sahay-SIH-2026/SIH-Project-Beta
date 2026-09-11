61f910bc4f782285c1681288ee282240962c6d45
Prateek Karna <prateek.karna03@gmail.com>
2026-09-12 04:05:24 +0530

    Fix upstream typings and missing recharts dependency

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -197,8 +197,8 @@ function ChartTooltipContent({
       {!nestLabel ? tooltipLabel : null}
       <div className="grid gap-1.5">
         {payload
-          .filter((item) => item.type !== "none")
-          .map((item, index) => {
+          .filter((item: any) => item.type !== "none")
+          .map((item: any, index: any) => {
             const key = `${nameKey ?? item.name ?? item.dataKey ?? "value"}`
             const itemConfig = getPayloadConfigFromPayload(config, item, key)
             const indicatorColor = color ?? item.payload?.fill ?? item.color
@@ -296,8 +296,8 @@ function ChartLegendContent({
       )}
     >
       {payload
-        .filter((item) => item.type !== "none")
-        .map((item, index) => {
+        .filter((item: any) => item.type !== "none")
+        .map((item: any, index: any) => {
           const key = `${nameKey ?? item.dataKey ?? "value"}`
           const itemConfig = getPayloadConfigFromPayload(config, item, key)
 
